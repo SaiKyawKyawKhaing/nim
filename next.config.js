0db28ecc0b6690946342f384543c59c97deff7e8
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-04-05 22:29:18 +0630

    fixed server.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  // Conditionally set basePath and assetPrefix for production (GitHub Pages)
-  basePath: process.env.NODE_ENV === 'production' ? "https://nextitmyanmar.com" : '', 
+  // Configure assetPrefix for production
+  basePath: '',
   assetPrefix: process.env.NODE_ENV === 'production' ? "https://nextitmyanmar.com" : '',
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
