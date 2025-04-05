4b5fdc3c70d7bd295d1d95cb5820141c2c394ee7
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-04-06 04:38:51 +0630

    rollback

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Conditionally set basePath and assetPrefix for production (GitHub Pages)
-  basePath: process.env.NODE_ENV === 'production' ? "https://digitechvault.com" : '', 
-  assetPrefix: process.env.NODE_ENV === 'production' ? "https://digitechvault.com" : '',
+  output: 'export',  // Set output to 'export'
+  basePath: process.env.NODE_ENV === 'production' ? "" : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? "" : '',
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
