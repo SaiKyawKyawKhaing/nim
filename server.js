c11dd2cd3cb95021f8aeff6efe9a49228ba5a9ad
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-04-05 22:43:35 +0630

    fixed server.js

--- server.js
+++ server.js
@@ -3,26 +3,17 @@ const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
-const app = next({ dev, hostname, port })
+const hostname = process.env.HOSTNAME || 'localhost'
+const port = process.env.PORT || 3000
+const app = next({ dev })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
- 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
+      await handle(req, res, parsedUrl)
     } catch (err) {
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500
@@ -34,6 +25,6 @@ app.prepare().then(() => {
       process.exit(1)
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
+      console.log(`> Ready on port ${port}`)
     })
 })
